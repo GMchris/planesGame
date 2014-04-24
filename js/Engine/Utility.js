6970d1a245b6527b0d9ba1a99ddaa78652c5e97d
Nikola Iliev <nikola.iliev93@gmail.com>
2014-04-24 21:15:15 +0300

    All ajax functions

--- js/Engine/Utility.js
+++ js/Engine/Utility.js
@@ -107,3 +107,52 @@ function getHighScore() {
     });
 }
 
+function isHighScore(score) {
+    var position = -1,
+        isHS = false;
+    $.ajax({
+        url: "http://bashibozuk.eu/games-score/?route=high-score/is-high-score&gameId=c4ca4238a0b923820dcc509a6f75849b&score=" + score,
+
+        // the name of the callback parameter, as specified by the YQL service
+        jsonp: "callback",
+
+        // tell jQuery we're expecting JSONP
+        dataType: "jsonp",
+
+        // tell YQL what we want and that we want JSON
+        data: {
+            q: "select title,abstract,url from search.news where query=\"cat\"",
+            format: "json"
+        },
+
+        // work with the response
+        success: function (response) {
+            position = response.data.position;
+        }
+    });
+    return position;
+}
+
+function saveHighScore(name, score) {
+    $.ajax({
+        url: "http://bashibozuk.eu/games-score/?route=high-score/save-high-score&gameId=c4ca4238a0b923820dcc509a6f75849b&score=" + score + "&player=" + encodeURIComponent(name),
+
+        // the name of the callback parameter, as specified by the YQL service
+        jsonp: "callback",
+
+        // tell jQuery we're expecting JSONP
+        dataType: "jsonp",
+
+        // tell YQL what we want and that we want JSON
+        data: {
+            q: "select title,abstract,url from search.news where query=\"cat\"",
+            format: "json"
+        },
+
+        // work with the response
+        success: function (response) {
+            console.log(response.data);
+        }
+    });
+}
+
