8be7e2a0ea957dd49a3bcdfefee9d6fc7e87f77e
Nikola Iliev <nikola.iliev93@gmail.com>
2014-04-26 13:58:24 +0300

    Kamikaze follows player

--- js/GameObjects/Planes/EnemyChasePlane.js
+++ js/GameObjects/Planes/EnemyChasePlane.js
@@ -16,14 +16,42 @@ EnemyChasePlane = EnemyPlane.extend({
             this.orientationDeg *= -1;
         }
 
+        //if (this.bottomCoord > playerBottom) {
+        //    this.div.style['-webkit-transform'] = 'rotate(' + (-this.orientationDeg) + 'deg)';
+		//	this.div.style['-moz-transform'] = 'rotate(' + (-this.orientationDeg) + 'deg)';
+		//	this.div.style['transform'] = 'rotate(' + (-this.orientationDeg) + 'deg)';
+        //} else {
+        //    this.div.style['-webkit-transform'] = 'rotate(' + (180 + this.orientationDeg) + 'deg)';
+		//	this.div.style['-moz-transform'] = 'rotate(' + (180 + this.orientationDeg) + 'deg)';
+		//	this.div.style['transform'] = 'rotate(' + (180 + this.orientationDeg) + 'deg)';
+        //}
+    },
+
+    drawHpBar: function () {
+        ctx.beginPath();
+        ctx.fillStyle = 'red';
+        ctx.rect(-this.width / 2, (-this.height / 2) - 5, this.width * (this.currentHealth / this.maxHealth), 5);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.lineWidth = 2;
+        ctx.rect(-this.width / 2, (-this.height / 2)  - 5, this.width, 5);
+        ctx.stroke();
+    },
+
+    move: function () {
+        var playerLeft = interactionManager.getPlayerLeftCoord(),
+            playerBottom = interactionManager.getPlayerBottomCoord();
+        ctx.save();
+        ctx.translate(this.leftCoord + this.width / 2, this.bottomCoord + this.height / 2);
+        //ctx.translate(this.leftCoord, this.bottomCoord); //kamikaze rotates around it's lower-left point, not around its center - feels unnatural
         if (this.bottomCoord > playerBottom) {
-            this.div.style['-webkit-transform'] = 'rotate(' + (-this.orientationDeg) + 'deg)';
-			this.div.style['-moz-transform'] = 'rotate(' + (-this.orientationDeg) + 'deg)';
-			this.div.style['transform'] = 'rotate(' + (-this.orientationDeg) + 'deg)';
+            ctx.rotate(degreeToRadian(this.orientationDeg));
         } else {
-            this.div.style['-webkit-transform'] = 'rotate(' + (180 + this.orientationDeg) + 'deg)';
-			this.div.style['-moz-transform'] = 'rotate(' + (180 + this.orientationDeg) + 'deg)';
-			this.div.style['transform'] = 'rotate(' + (180 + this.orientationDeg) + 'deg)';
+            ctx.rotate(degreeToRadian(180 - this.orientationDeg));
         }
+        ctx.drawImage(this.img, -this.width / 2, -this.height / 2);
+        //ctx.drawImage(this.img, 0, 0);
+        this.drawHpBar();
+        ctx.restore();
     }
 });
