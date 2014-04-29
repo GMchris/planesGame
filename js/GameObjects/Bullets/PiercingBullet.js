87deba44cb57227a2b7b5bdae9ff95ec5198adfb
Kristian Ignatov <christianignatov@gmail.com>
2014-04-29 18:21:12 +0300

    Penetrating bullet color and icon fix

--- js/GameObjects/Bullets/PiercingBullet.js
+++ js/GameObjects/Bullets/PiercingBullet.js
@@ -3,7 +3,7 @@
         var piercingWidth = width ? width : 15,
             piercingHeight = height ? height : 4;
         this._super(left, bottom, orientationDeg, owner, piercingWidth, piercingHeight);
-        this.bulletColor = '#78f1ff';
+        this.bulletColor = '#a34ba9';
         this.enemiesHit = [];
     },
 
