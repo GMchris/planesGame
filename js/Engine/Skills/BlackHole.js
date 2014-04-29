7f65c7f163d1fbda1c38fcf2cbe647ec7ec94e6b
Nikola Iliev <nikola.iliev93@gmail.com>
2014-04-29 18:05:15 +0300

    Black Hole cd fixed

--- js/Engine/Skills/BlackHole.js
+++ js/Engine/Skills/BlackHole.js
@@ -1,6 +1,6 @@
 ﻿BlackHole = Skill.extend({
     init: function (plane, index) {
-        this._super("Black Hole", plane, 1, 3000, "blackHoleIcon", index); //plane using the skill, duration, cooldown
+        this._super("Black Hole", plane, 1, 40000, "blackHoleIcon", index); //plane using the skill, duration, cooldown
     },
 
     activate: function () {
